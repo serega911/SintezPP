4088f5185822432684254846522af6afa225e71c
serega911 <pasechnuy.sergey@gmail.com>
2016-03-24 23:56:05 +0200

    work started

--- Libraries/TIOFileManager.cpp
+++ Libraries/TIOFileManager.cpp
@@ -1,10 +1,28 @@
 #include "../Libraries/TIOFileManager.h"
+#include <direct.h>
 
-const std::string						pss::TIOFileManager::s_folder = "../results";
+const std::string								pss::TIOFileManager::s_folder = "..//Results";
 
 std::string pss::TIOFileManager::getFolder()
 {
 	return s_folder;
+}
+
+void pss::TIOFileManager::writeToFile(eOutputFileType type, const TCode & code)
+{
+	auto file = m_oFiles.find(type);
+	if (file != m_oFiles.end())
+	{
+		code.writeCodeToFile(*(file->second));
+	}
+	else
+	{
+		std::ofstream* out = new std::ofstream;
+		std::string fullName = s_folder + "//" + m_fileNames.at(type);
+		out->open(fullName.c_str(), std::ofstream::out);
+		m_oFiles.insert({type, out});
+		code.writeCodeToFile(*out);
+	}
 }
 
 pss::TIOFileManager::TIOFileManager()
@@ -19,9 +37,18 @@ pss::TIOFileManager* pss::TIOFileManager::getInstance()
 
 pss::TIOFileManager::~TIOFileManager()
 {
+	for (auto& it : m_oFiles)
+		delete (it.second);
+	m_oFiles.clear();
 }
 
 void pss::TIOFileManager::init()
 {
-
+	m_fileNames[eOutputFileType::FAIL_0] = "failed_0.log";
+	m_fileNames[eOutputFileType::FAIL_FREE] = "failed_free.log";
+	m_fileNames[eOutputFileType::FAIL_N] = "failed_N.log";
+	m_fileNames[eOutputFileType::FAIL_REPETTION] = "failed_repetition.log";
+	m_fileNames[eOutputFileType::INITIAL_DATA] = "initial_data.init";
+	m_fileNames[eOutputFileType::DONE] = "done.pkp";
+	_mkdir(s_folder.c_str());
 }
--- Libraries/TIOFileManager.h
+++ Libraries/TIOFileManager.h
@@ -2,23 +2,43 @@
 
 #include <string>
 #include <fstream>
+#include <map>
+
+#include "../Libraries/TCode.h"
 
 namespace pss
 {
 	class TIOFileManager
 	{
 	public:
+
+		enum class eOutputFileType
+		{
+			INITIAL_DATA,	// initial data - W N Count_L Count_F Count_B
+			DONE,			// done
+			FAIL_N,			// wrong number of elements for brakes and frictions
+			FAIL_0,			// links from in to out or between elements of one planetary mechanism
+			FAIL_FREE,		// after setting frictions and brakes there are free elements
+			FAIL_REPETTION	// scheme has already exist 
+		};
+
 		static TIOFileManager*					getInstance();
 		~TIOFileManager();
 		void									init();
 		std::string								getFolder();
 
+		void									writeToFile(eOutputFileType type, const TCode & code);
+
 	private:
+		std::map<eOutputFileType, std::string>	m_fileNames;
+
 												TIOFileManager();
 												TIOFileManager(const TIOFileManager&) = delete;
 		TIOFileManager&							operator=(TIOFileManager&) = delete;
 
 		static const std::string				s_folder;
+		std::map<eOutputFileType, std::ofstream*> m_oFiles;
+
 		
 	};
 
