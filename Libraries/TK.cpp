36fa4c85ed684675da8e50eb31a5228d8022c68a
serega911 <pasechnuy.sergey@gmail.com>
2016-04-06 22:38:41 +0300

    changed intervals in TK::next

--- Libraries/TK.cpp
+++ Libraries/TK.cpp
@@ -7,9 +7,22 @@
 pss::TK::TK(float beginNegative, float endNegative, float beginPositive, float endPositive, float dK) :
 m_beginNegative(beginNegative), m_endNegative(endNegative), m_beginPositive(beginPositive), m_endPositive(endPositive), m_dK(dK), m_isFinded(false)
 {
+	if (m_beginNegative <= -4.5f && m_endNegative >= -4.5f)
+		m_midNegative = -4.5f;
+	else
+		m_midNegative = (m_beginNegative + m_endNegative) / 2.0f;
+	if (m_beginPositive <= 4.5f && m_endPositive >= 4.5f)
+		m_midPositive = 4.5f;
+	else
+		m_midPositive = (m_beginPositive + m_endPositive) / 2.0f;
+
+
 	m_K.resize(pss::TSingletons::getInstance()->getNumberOfPlanetaryGears());
 	for (auto& it : m_K)
-		it = beginNegative;
+		it = m_midNegative;
+	m_KInterval.resize(pss::TSingletons::getInstance()->getNumberOfPlanetaryGears());
+	for (auto& it : m_KInterval)
+		it = eInterval::MID_NEGATIVE;
 }
 
 const float pss::TK::operator[](int i) const
@@ -21,17 +34,33 @@ bool pss::TK::next()
 {
 	int k = (int)m_K.size();
 	for (int i = k - 1; i >= 0; --i)
-			if (m_K[i] <= m_endPositive - m_dK)
+	{
+		if (m_K[i] > m_midNegative - m_dK  && m_KInterval[i] == eInterval::BEG_NEGATIVE)
+		{
+			m_K[i] = m_midNegative;
+			m_KInterval[i] = eInterval::MID_NEGATIVE;
+		}
+		else
+		{
+			m_K[i] += m_dK;
+			if (m_K[i] > m_endNegative && m_KInterval[i] == eInterval::MID_NEGATIVE)
 			{
-				m_K[i] += m_dK;
-				if (m_K[i] > m_endNegative && m_K[i] < m_beginPositive)
-					m_K[i] = m_beginPositive;
-				return true;
+				m_K[i] = m_beginPositive;
+				m_KInterval[i] = eInterval::BEG_POSITIVE;
 			}
-			else
+			if (m_K[i] > m_midPositive && m_KInterval[i] == eInterval::BEG_POSITIVE)
+			{
+				m_K[i] = m_midPositive;
+				m_KInterval[i] = eInterval::MID_POSITIVE;
+			}
+			if (m_K[i] > m_endPositive && m_KInterval[i] == eInterval::MID_POSITIVE)
 			{
 				m_K[i] = m_beginNegative;
+				m_KInterval[i] = eInterval::BEG_NEGATIVE;
 			}
+			return true;
+		}
+	}
 	return false;
 }
 
--- Libraries/TK.h
+++ Libraries/TK.h
@@ -9,13 +9,25 @@ namespace pss
 	class TK: public IContainer
 	{
 	private:
+		enum class eInterval
+		{
+			BEG_NEGATIVE,
+			MID_NEGATIVE,
+			BEG_POSITIVE,
+			MID_POSITIVE
+		};
+
+
 		float									m_beginNegative;
+		float									m_midNegative;
 		float									m_endNegative;
 		float									m_beginPositive;
+		float									m_midPositive;
 		float									m_endPositive;
 		float									m_dK;
 		bool									m_isFinded;
 		std::vector<float>						m_K;
+		std::vector<eInterval>					m_KInterval;
 
 	public:
 		TK() = delete;
